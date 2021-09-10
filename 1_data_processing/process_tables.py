cda97269fb16145a8bb5af900eb06d501ef0b5b5
mgbarois <mgbarois@gmail.com>
2021-09-10 12:22:16 +0200

    Drop project id column in final df

--- 1_data_processing/process_tables.py
+++ 1_data_processing/process_tables.py
@@ -27,67 +27,19 @@ def calc_months(acqu):
 
 grouped_df['months_acquired'] = grouped_df['acquisition_date'].apply(calc_months)
 
-# Filter projects under 11 months
+# Filter out projects under 11 months
 grouped_df = grouped_df[grouped_df['months_acquired'] > 11]
 
 # Sort alphabetically by project name and reindex
-grouped_df = grouped_df.sort_values('project_name').reset_index(drop=True)
+grouped_df = grouped_df.sort_values('project_name').reset_index(drop=True).drop(columns=['id'])
 
 # Save to excel
 grouped_df.to_excel("Exports/project_view_table.xlsx")
 
-# (Save raw WTG table to excel for coordinate retrieval from the backend)
+# --- Coordinates
+
+# Save raw WTG table to excel for coordinate retrieval from the backend
 wtg_df.to_excel("Exports/WTG_raw_table.xlsx")
-
-
-# joined['kW'] = joined.groupby('project_id')['kW'].agg(sum)
-# print("Kw:", joined)
-
-
-# acqu_group = joined.groupby('project_id')['acquisition_date'].apply(lambda x : calc_months(x))
-
-
-# print(acqu_group)
-
-
-# Create Project View table
-# Initialize working df
-# wtg_info = pandas.DataFrame()
-
-# # Iterate project ids
-# for p_id in projects['id']:
-
-#     # Retrive corresponding wtgs
-#     corress_wtgs = wtgs[wtgs['project_id'] == p_id]
-
-#     # Concatenate WTG numbers of all corresponding WTGs
-#     wtg_numbers = ';'.join(corress_wtgs['WTG_number'].tolist())
-
-#     # Sum up kW total of all corresponding WTGs
-#     kws = sum(corress_wtgs['kW'])
-
-#     # Calculate months since acquisition
-#     acqu_date = projects[projects['id'] == p_id]['acquisition_date'].values[0]
-#     acqu_date = pandas.to_datetime(acqu_date, format="%Y-%m-%d")
-#     today_date = datetime.datetime.now()
-#     months = (today_date.year - acqu_date.year) * 12 + (today_date.month - acqu_date.month)
-
-#     # Append data to working df
-#     wtg_info = wtg_info.append([{'project_id': p_id, 'WTG_numbers': wtg_numbers, 'kW': kws, 'months_acquired': months}])
-
-# # Join working df with project df
-# project_view = projects.join(wtg_info.set_index('project_id'), on='id')
-
-# # Filter projects over 11 months
-# project_view = project_view[project_view['months_acquired'] > 11]
-
-# # Sort projects by name
-# project_view = project_view.sort_values('project_name')
-
-# # Resent indexes
-# project_view = project_view.reset_index(drop=True)
-
-# # Save df to excel file
 
 
 
